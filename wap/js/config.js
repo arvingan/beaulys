182a37de179ab9879180773abb6ee24e1df9c77f
kimmykuang <kimmykuang@hotmail.com>
2016-06-26 21:35:02 +0800

    modify config

--- wap/js/config.js
+++ wap/js/config.js
@@ -1,5 +1,5 @@
 var SiteUrl = "http://"+window.location.host+"/shop/shop";//"http://v3.haoid.cn/mobile";
-var ApiUrl = "http://"+window.location.host+"/shop/mobile";//"http://v3.haoid.cn/mobile";
+var ApiUrl = "http://api.dev.beaulys.com";//"http://v3.haoid.cn/mobile";
 var pagesize = 10;
 var WapSiteUrl = "http://"+window.location.host+"/shop/wap";//"http://v3.haoid.cn/wap";
 var IOSSiteUrl = "http://"+window.location.host+"/shop/app.ipa";//"http://v3.haoid.cn/app.ipa";
